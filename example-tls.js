bae63b2a3622b69f3853d647ac55fad7b9f53004
Shane Osbourne <shane@wearejh.com>
2015-02-23 16:35:56 +0000

    playground

---
+++ example-tls.js
@@ -0,0 +1,102 @@
+var ThrottleGroup = require('stream-throttle').ThrottleGroup;
+var net           = require('net');
+var Immutable     = require('immutable');
+
+require("browser-sync")({
+    server: "app",
+    files: "app/**",
+    open: false,
+    https: true,
+    middleware: function (req, res, next) {
+        res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+        res.setHeader("Pragma", "no-cache");
+        res.setHeader("Expires", "0");
+        next();
+    },
+    snippetOptions: {
+        blacklist: ["/", "/**"]
+    }
+}, throttle);
+
+var speeds = {
+    "3g": 75,
+    "edge": 25,
+    "gprs": 5
+};
+
+/**
+ *
+ */
+function throttle (err, bs) {
+
+    var options = {
+        local_host:  'localhost',
+        remote_host: 'localhost',
+        upstream:    10*1024,
+        downstream:  speeds["edge"] * 1024,
+        keepalive:   false
+    };
+
+    var upThrottle = new ThrottleGroup({ rate: options.upstream });
+    var downThrottle = new ThrottleGroup({ rate: options.downstream });
+
+    var url = require("url").parse(bs.options.getIn(["urls", "local"]));
+
+    var utils = require("browser-sync/lib/server/utils.js");
+
+    var certs = utils.getKeyAndCert(Immutable.Map({scheme: "https"}));
+    var opts = {
+        key: certs.key,
+        cert: certs.cert,
+        allowHalfOpen: true,
+        rejectUnauthorized: false
+    };
+
+    var server = require("tls").createServer(opts, function (local) {
+
+        var remote = require("tls").connect({
+            host: url.hostname,
+            port: url.port,
+            allowHalfOpen: true,
+            rejectUnauthorized: false
+        }, function () {
+            console.log(remote.authorized);
+        });
+
+        var localThrottle = upThrottle.throttle();
+        var remoteThrottle = downThrottle.throttle();
+
+        local
+            .pipe(localThrottle)
+            .pipe(remote);
+
+        local.on('error', function(err) {
+            //console.log("Local Error: ", err);
+            remote.destroy();
+            local.destroy();
+        });
+
+        remote
+            .pipe(remoteThrottle)
+            .pipe(local);
+
+        remote.on('error', function(err) {
+            //console.log("Remote err: ", err);
+            local.destroy();
+            remote.destroy();
+        });
+    });
+
+    server.listen(8989, "0.0.0.0");
+
+    server.on('listening', function() {
+        require("opn")("https://localhost:8989");
+        //var localAddr = options.local_host + ':' + options.local_port;
+        //var remoteAddr = options.remote_host + ':' + options.remote_port;
+        //grunt.log.writeln('Throttling connections to ' + remoteAddr + ', go to ' + localAddr);
+    });
+
+    server.on('error', function(err) {
+        console.log(err);
+    });
+}
